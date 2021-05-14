417f6251fd92c10af9a6fc0284076dbe0f138ece
raulcrespofer <76496648+raulcrespofer@users.noreply.github.com>
2021-05-14 09:48:37 +0200

    Update leerFicheroXML.js

--- Ejercicio1/leerFicheroXML.js
+++ Ejercicio1/leerFicheroXML.js
@@ -3,7 +3,7 @@ function gestionarFicheroXML(xmlDoc){
 	let alumnos = xmlDoc.querySelectorAll("libro")
 	for(let i=0; i<alumnos.length; i++)
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + alumnos[i].textContent + "</p>"
-	alert(xmlDoc)
+	
 	
 }
 

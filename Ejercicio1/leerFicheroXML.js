34011a9c9888808246c1293141ec35e1c37cf7de
raulcrespofer <76496648+raulcrespofer@users.noreply.github.com>
2021-05-19 18:30:32 +0200

    Add files via upload

--- Ejercicio1/leerFicheroXML.js
+++ Ejercicio1/leerFicheroXML.js
@@ -1,10 +1,8 @@
 function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
 	let alumnos = xmlDoc.querySelectorAll("libro")
-	for(let i=0; i<alumnos.length; i++)
+	for(let i=0; i<libros.length; i++)
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + alumnos[i].textContent + "</p>"
-	
-	
 }
 
 

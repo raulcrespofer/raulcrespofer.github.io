d90541f09c2d8929b43bc40806ef2e8f1d6571f6
raulcrespofer <76496648+raulcrespofer@users.noreply.github.com>
2021-05-20 22:38:42 +0200

    Add files via upload

--- Ejercicio2/leerFicheroXML.js
+++ Ejercicio2/leerFicheroXML.js
@@ -2,16 +2,16 @@ function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
 	let mensajes = xmlDoc.querySelectorAll("mensaje")
 	let conversacion = xmlDoc.querySelector("conversacion")
-	for(let i=0; i<mensajes.length; i++){
+	for(let i=0; i<primero.length&&derecha.length; i++){
 		
-		capaVacia.innerHTML = capaVacia.innerHTML + "<main>" + conversacion[i].textContent + "</main>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<main class='conversacion'>" + "</main>"
 
 			capaVacia.innerHTML = capaVacia.innerHTML + "<p class='verde'>" + mensajes[i].textContent + "</p>"
 
 			capaVacia.innerHTML = capaVacia.innerHTML + "<p class='azul'>" + mensajes[i].textContent + "</p>"
 	
 }
-}
+	}
 
 
 	loadDocA("mensajes.xml","xml");
